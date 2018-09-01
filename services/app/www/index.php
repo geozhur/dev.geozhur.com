ce95625305c1253ad409d7ac22de512debe453f4
Santa Klaus <egorich2004@mail.ru>
2018-09-01 18:03:38 +0200

    slim

--- services/app/www/index.php
+++ services/app/www/index.php
@@ -1,1 +1,13 @@
-<?php phpinfo();
+<?php 
+
+require __DIR__ . '/../vendor/autoload.php';
+// instantiate the App object
+$app = new \Slim\App();
+
+// Add route callbacks
+$app->get('/', function ($request, $response, $args) {
+    return $response->withStatus(200)->write(' HELLO SLIM ');
+});
+
+// Run application
+$app->run();
