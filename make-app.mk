25dbd34207a01a5dfbfedc04d8d7aec8ec99700f
Santa Klaus <egorich2004@mail.ru>
2018-09-02 17:19:46 +0200

    with compresor from source

--- make-app.mk
+++ make-app.mk
@@ -4,13 +4,13 @@ app:
 	docker-compose up
 
 app-build:
-	docker-compose build
+	docker-compose build 
 
 app-bash:
-	docker-compose run --user=$(USER) app bash
+	docker-compose run app bash
 
 app-setup: development-setup-env app-build
-	docker-compose run --user=$(USER) app bin/setup
+	docker-compose run app
 
 development-setup-env:
 	ansible-playbook ansible/development.yml -i ansible/development -vv
